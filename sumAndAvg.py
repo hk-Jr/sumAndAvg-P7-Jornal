09b684bb67b46ead46c418c1beb6349aacc9e0ae
kishna h k <khk803123@gmail.com>
2025-11-27 17:15:40 +0530

    added max and min.

--- sumAndAvg.py
+++ sumAndAvg.py
@@ -14,10 +14,13 @@ else:
     
 total = sum(scores)
 average = total / len(scores)
-
+maximum = max(scores)
+minimum = min(scores)
     
 print(f"Scores: {scores}")
 print(f"Sum of scores: {total}")
 print(f"Average of scores: {average:.2f}")
+print(f"Maximum score: {maximum}")
+print(f"Minimum score: {minimum}")
 
 
