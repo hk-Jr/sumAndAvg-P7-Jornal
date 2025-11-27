ddf9ad9b3187c57068d419dd587716ce372729a8
kishna h k <khk803123@gmail.com>
2025-11-27 17:12:57 +0530

    this cals sum and avg

---
+++ sumAndAvg.py
@@ -0,0 +1,23 @@
+# array_scores.py
+
+import sys
+
+if len(sys.argv)>1:
+  script_name=sys.argv[0]
+  scores = [int(arg) for arg in sys.argv[1:]]
+  print("user details inputted.")
+
+else:
+    print("default details.")
+    script_name=sys.argv[0]
+    scores=[10,20,30,40]
+    
+total = sum(scores)
+average = total / len(scores)
+
+    
+print(f"Scores: {scores}")
+print(f"Sum of scores: {total}")
+print(f"Average of scores: {average:.2f}")
+
+
